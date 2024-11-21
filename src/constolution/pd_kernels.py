880dd7e202d6fa4e3d24d4e371429d84728f938c
esteban1324 <estebanlinarez@ymail.com>
2024-11-20 19:42:59 -0500

    horizontal adjusted

--- src/constolution/pd_kernels.py
+++ src/constolution/pd_kernels.py
@@ -13,8 +13,19 @@ def gaussian_kernel(height, width, sigma=1.0):
 
     return kernel
 
+# nd array return type 
 def horizontal_kernel(height, width):
-    pass 
+    h_k = np.zeros((height, width))
+    # fill rows between -2 to 2 horizontally across
+    # weights on top are 1 and bottom -1
+    half_width = width // 2
+    h_k[:(half_width) - (1 - width % 2), :] = 1
+    h_k[half_width + 1:, :] = -1 
+
+    
+    return h_k 
+
+print(horizontal_kernel(6,6))
 
 def box_kernel(height, width):
     pass
