ec5afec283e3bb2c9ba92af0d7bb04fbb3162eae
Preston DeLeo <121273342+prestondeleo@users.noreply.github.com>
2024-11-25 22:13:48 -0500

    Adding block class outline

---
+++ src/constolution/block.py
@@ -0,0 +1,15 @@
+class earlyBlock:
+
+    "gabor, sobel, schmidit, gaussian, *five_one"
+
+    def forward():
+        pass
+
+
+class middleBlock:
+
+    "kernel1, kernel2, kernel3, kernel4, kernel5"
+
+
+    def forward():
+        pass
