d22491f3e55c4add503c9dc3de83fd45778b33ec
Preston DeLeo <121273342+prestondeleo@users.noreply.github.com>
2024-11-27 00:49:03 -0500

    Adding fix for base vgg16

--- evaluation/mobilenet/base_vgg.py
+++ evaluation/mobilenet/base_vgg.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torchvision.transforms as transforms
+import torch.optim as optim
+import torchvision
 
 #set device
 device = torch.device(
@@ -13,6 +15,19 @@ transform = transforms.Compose(
     [transforms.ToTensor(),
      transforms.Normalize((0.5, 0.5, 0.5), (0.2, 0.2, 0.2))])
 
+trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
+                                        download=True, transform=transform)
+trainloader= torch.utils.data.DataLoader(trainset, batch_size=16,
+                                        shuffle=True, num_workers=2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                    download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=16,
+                                        shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+        'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
 
 class VGG16(nn.Module):
 
@@ -20,59 +35,62 @@ class VGG16(nn.Module):
         super(VGG16, self).__init__()
         self.blck1  = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=64, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2)
         )
 
         self.blck2 = nn.Sequential(
             nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2)
         )
 
         self.blck3 = nn.Sequential(
             nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=256, out_channels=256, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2)
         )
 
         self.blck4 = nn.Sequential(
             nn.Conv2d(in_channels=256, out_channels=512, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2)
         )
 
         self.blck5 = nn.Sequential(
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Conv2d(in_channels=512, out_channels=512, kernel_size=(3,3), padding=1),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.MaxPool2d(2, 2)
         )
 
-        self.blck6 = nn.Seqential(
-            nn.Linear(25088, 4096),
-            nn.Relu(inplace=True),
+        self.blck6 = nn.Sequential(
+            #nn.Linear(25088, 4096),
+            #nn.Linear(2048, 4096),
+            ##CHANGED ABOVE FOR CIFAR
+            nn.Linear(512, 4096),
+            nn.ReLU(inplace=True),
             nn.Dropout(0.5),
             nn.Linear(4096,4096),
-            nn.Relu(inplace=True),
+            nn.ReLU(inplace=True),
             nn.Dropout(0.5),
-            #nn.Linear(4096,1000)
             nn.Linear(4096,number_classes)
+
         )
 
     def forward(self, input):
@@ -80,9 +98,9 @@ class VGG16(nn.Module):
         out = self.blck2(out)
         out = self.blck3(out)
         out = self.blck4(out)
-        out = torch.flatten(out, 1)
         out = self.blck5(out)
+        out = torch.flatten(out, 1)
+        out = self.blck6(out)
         out = torch.softmax(out, dim=1)
         return out
 
-    
