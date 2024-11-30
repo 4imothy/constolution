319dde27efb822315997793effd5769691e9575b
Timothy Cronin <tcroniniv@gmail.com>
2024-11-30 13:09:54 -0700

    add caltech256

--- evaluation/utils.py
+++ evaluation/utils.py
@@ -1,7 +1,10 @@
 import os
 import torchvision
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, random_split, Dataset
 import torchvision.transforms as transforms
+
+IMAGENET_MEAN = [0.485, 0.456, 0.406]
+IMAGENET_STDDEV = [0.229, 0.224, 0.225]
 
 def load_cifar10_data(batch_size, image_size):
     train_transforms = transforms.Compose([
@@ -24,6 +27,58 @@ def load_cifar10_data(batch_size, image_size):
     val_dataset = torchvision.datasets.CIFAR10(
         root=data_root, train=False, transform=val_transforms, download=True
     )
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
+
+    return train_loader, val_loader
+
+class TransformDataset(Dataset):
+    def __init__(self, dataset, transform):
+        self.dataset = dataset
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.dataset)
+
+    def __getitem__(self, idx):
+        image, label = self.dataset[idx]
+        if self.transform:
+            image = self.transform(image)
+        return image, label
+
+def to_rgb(x):
+    return x.convert('RGB')
+
+def load_caltech256_data(batch_size, image_size, split_ratio=0.8):
+    train_transforms = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.RandomHorizontalFlip(),
+        transforms.Lambda(to_rgb),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STDDEV),
+    ])
+
+    val_transforms = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.Lambda(to_rgb),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STDDEV),
+    ])
+
+    pfsdir = os.getenv('PFSDIR')
+    data_root = os.path.join(pfsdir, 'datasets') if pfsdir else './datasets'
+
+    full_dataset = torchvision.datasets.Caltech256(
+        root=data_root, download=True
+    )
+
+    train_size = int(split_ratio * len(full_dataset))
+    val_size = len(full_dataset) - train_size
+    train_dataset, val_dataset = random_split(full_dataset, [train_size, val_size])
+
+    train_dataset = TransformDataset(train_dataset, train_transforms)
+    val_dataset = TransformDataset(val_dataset, val_transforms)
 
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4, pin_memory=True)
     val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, num_workers=4, pin_memory=True)
