8059b1d50c7381318090d898863b93ac9fb099b2
RithvikKasarla <rithvikrk719@gmail.com>
2024-11-30 18:34:30 -0500

    updates

--- evaluation/resnet/resnet-18.py
+++ evaluation/resnet/resnet-18.py
@@ -10,7 +10,7 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class ResBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
+        super(ResBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride,
                                padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(planes)
