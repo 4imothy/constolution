9e280f10eb56c8d7ce9d192dfeaf0f9c090f78ea
Timothy Cronin <tcroniniv@gmail.com>
2024-12-01 00:31:09 -0700

    add pd inceptionv3

--- evaluation/inception_v3/model_with_pd.py
+++ evaluation/inception_v3/model_with_pd.py
@@ -3,6 +3,7 @@ from typing import List, Optional, Tuple
 from torch import nn
 from torch import Tensor
 import torch.nn.functional as F
+import constolution as ct
 
 class Inception3(nn.Module):
     def __init__(
@@ -16,19 +17,21 @@ class Inception3(nn.Module):
         self.aux_logits = aux_logits
         self.transform_input = transform_input
         self.Conv2d_1a_3x3 = BasicConv2d(3, 32, kernel_size=3, stride=2)
-        self.Conv2d_2a_3x3 = BasicConv2d(32, 32, kernel_size=3)
+        self.Conv2d_2a_3x3 = ct.EarlyEdgeInception(32, 7, kernel_size=3,
+                                                   weighted_convolution=True,
+                                                   weighted_out_channels=32)
         self.Conv2d_2b_3x3 = BasicConv2d(32, 64, kernel_size=3, padding=1)
         self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
         self.Conv2d_3b_1x1 = BasicConv2d(64, 80, kernel_size=1)
         self.Conv2d_4a_3x3 = BasicConv2d(80, 192, kernel_size=3)
         self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2)
         self.Mixed_5b = InceptionA(192, pool_features=32)
-        self.Mixed_5c = InceptionA(256, pool_features=64)
-        self.Mixed_5d = InceptionA(288, pool_features=64)
+        self.Mixed_5c = PDInceptionA(256, pool_features=64)
+        self.Mixed_5d = PDInceptionA(288, pool_features=64)
         self.Mixed_6a = InceptionB(288)
-        self.Mixed_6b = InceptionC(768, channels_7x7=128)
-        self.Mixed_6c = InceptionC(768, channels_7x7=160)
-        self.Mixed_6d = InceptionC(768, channels_7x7=160)
+        self.Mixed_6b = PDInceptionC(768, channels_7x7=128)
+        self.Mixed_6c = PDInceptionC(768, channels_7x7=160)
+        self.Mixed_6d = PDInceptionC(768, channels_7x7=160)
         self.Mixed_6e = InceptionC(768, channels_7x7=192)
         self.AuxLogits: Optional[nn.Module] = None
         if aux_logits:
@@ -41,7 +44,7 @@ class Inception3(nn.Module):
         self.fc = nn.Linear(2048, num_classes)
         for m in self.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-                stddev = float(m.stddev) if hasattr(m, "stddev") else 0.1
+                stddev = float(m.stddev) if hasattr(m, 'stddev') else 0.1
                 torch.nn.init.trunc_normal_(m.weight, mean=0.0, std=stddev, a=-2, b=2)
             elif isinstance(m, nn.BatchNorm2d):
                 nn.init.constant_(m.weight, 1)
@@ -119,6 +122,48 @@ class BasicConv2d(nn.Module):
         x = self.bn(x)
         return F.relu(x, inplace=True)
 
+class BasicPDConv2d(nn.Module):
+    def __init__(self, type: ct.Kernels, in_channels: int, out_channels: int, **kwargs) -> None:
+        super().__init__()
+        self.conv = ct.Constolution2D(type, in_channels, out_channels, bias=False, **kwargs)
+        self.bn = nn.BatchNorm2d(out_channels, eps=0.001)
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self.conv(x)
+        x = self.bn(x)
+        return F.relu(x, inplace=True)
+
+
+class PDInceptionA(nn.Module):
+    def __init__(
+        self, in_channels: int, pool_features: int) -> None:
+        super().__init__()
+        self.branch1x1 = BasicConv2d(in_channels, 64, kernel_size=1)
+
+        self.branch5x5_1 = BasicConv2d(in_channels, 48, kernel_size=1)
+        self.branch5x5_2 = BasicPDConv2d(ct.Kernels.Box, 48, 64, kernel_size=5, padding=2)
+
+        self.branch3x3dbl_1 = BasicConv2d(in_channels, 64, kernel_size=1)
+        self.branch3x3dbl_2 = BasicPDConv2d(ct.Kernels.Average, 64, 96, kernel_size=3, padding=1)
+        self.branch3x3dbl_3 = BasicConv2d(96, 96, kernel_size=3, padding=1)
+
+        self.branch_pool = BasicConv2d(in_channels, pool_features, kernel_size=1)
+
+    def forward(self, x: Tensor) -> Tensor:
+        branch1x1 = self.branch1x1(x)
+
+        branch5x5 = self.branch5x5_1(x)
+        branch5x5 = self.branch5x5_2(branch5x5)
+
+        branch3x3dbl = self.branch3x3dbl_1(x)
+        branch3x3dbl = self.branch3x3dbl_2(branch3x3dbl)
+        branch3x3dbl = self.branch3x3dbl_3(branch3x3dbl)
+
+        branch_pool = F.avg_pool2d(x, kernel_size=3, stride=1, padding=1)
+        branch_pool = self.branch_pool(branch_pool)
+
+        outputs = [branch1x1, branch5x5, branch3x3dbl, branch_pool]
+        return torch.cat(outputs, 1)
 
 class InceptionA(nn.Module):
     def __init__(
@@ -135,7 +180,7 @@ class InceptionA(nn.Module):
 
         self.branch_pool = BasicConv2d(in_channels, pool_features, kernel_size=1)
 
-    def _forward(self, x: Tensor) -> List[Tensor]:
+    def forward(self, x: Tensor) -> Tensor:
         branch1x1 = self.branch1x1(x)
 
         branch5x5 = self.branch5x5_1(x)
@@ -149,10 +194,6 @@ class InceptionA(nn.Module):
         branch_pool = self.branch_pool(branch_pool)
 
         outputs = [branch1x1, branch5x5, branch3x3dbl, branch_pool]
-        return outputs
-
-    def forward(self, x: Tensor) -> Tensor:
-        outputs = self._forward(x)
         return torch.cat(outputs, 1)
 
 
@@ -181,6 +222,45 @@ class InceptionB(nn.Module):
         outputs = self._forward(x)
         return torch.cat(outputs, 1)
 
+class PDInceptionC(nn.Module):
+    def __init__(
+        self, in_channels: int, channels_7x7: int) -> None:
+        super().__init__()
+        self.branch1x1 = BasicConv2d(in_channels, 192, kernel_size=1)
+
+        c7 = channels_7x7
+        self.branch7x7_1 = BasicConv2d(in_channels, c7, kernel_size=1)
+        self.branch7x7_2 = BasicPDConv2d(ct.Kernels.Schmid, c7, c7, kernel_size=(1, 7), padding=(0, 3))
+        self.branch7x7_3 = BasicConv2d(c7, 192, kernel_size=(7, 1), padding=(3, 0))
+
+        self.branch7x7dbl_1 = BasicConv2d(in_channels, c7, kernel_size=1)
+        self.branch7x7dbl_2 = BasicPDConv2d(ct.Kernels.Box, c7, c7, kernel_size=(7, 1), padding=(3, 0))
+        self.branch7x7dbl_3 = BasicConv2d(c7, c7, kernel_size=(1, 7), padding=(0, 3))
+        self.branch7x7dbl_4 = BasicPDConv2d(ct.Kernels.Gabor, c7, c7, kernel_size=(7, 1), padding=(3, 0))
+        self.branch7x7dbl_5 = BasicConv2d(c7, 192, kernel_size=(1, 7), padding=(0, 3))
+
+        self.branch_pool = BasicConv2d(in_channels, 192, kernel_size=1)
+
+    def forward(self, x: Tensor) -> Tensor:
+        branch1x1 = self.branch1x1(x)
+
+        branch7x7 = self.branch7x7_1(x)
+        branch7x7 = self.branch7x7_2(branch7x7)
+        branch7x7 = self.branch7x7_3(branch7x7)
+
+        branch7x7dbl = self.branch7x7dbl_1(x)
+        branch7x7dbl = self.branch7x7dbl_2(branch7x7dbl)
+        branch7x7dbl = self.branch7x7dbl_3(branch7x7dbl)
+        branch7x7dbl = self.branch7x7dbl_4(branch7x7dbl)
+        branch7x7dbl = self.branch7x7dbl_5(branch7x7dbl)
+
+        branch_pool = F.avg_pool2d(x, kernel_size=3, stride=1, padding=1)
+        branch_pool = self.branch_pool(branch_pool)
+
+        outputs = [branch1x1, branch7x7, branch7x7dbl, branch_pool]
+        return torch.cat(outputs, 1)
+
+
 class InceptionC(nn.Module):
     def __init__(
         self, in_channels: int, channels_7x7: int) -> None:
@@ -200,7 +280,7 @@ class InceptionC(nn.Module):
 
         self.branch_pool = BasicConv2d(in_channels, 192, kernel_size=1)
 
-    def _forward(self, x: Tensor) -> List[Tensor]:
+    def forward(self, x: Tensor) -> Tensor:
         branch1x1 = self.branch1x1(x)
 
         branch7x7 = self.branch7x7_1(x)
@@ -217,12 +297,7 @@ class InceptionC(nn.Module):
         branch_pool = self.branch_pool(branch_pool)
 
         outputs = [branch1x1, branch7x7, branch7x7dbl, branch_pool]
-        return outputs
-
-    def forward(self, x: Tensor) -> Tensor:
-        outputs = self._forward(x)
         return torch.cat(outputs, 1)
-
 
 class InceptionD(nn.Module):
     def __init__(self, in_channels: int) -> None:
@@ -324,4 +399,5 @@ class InceptionAux(nn.Module):
         x = self.fc(x)
         # N x 1000
         return x
+
 
