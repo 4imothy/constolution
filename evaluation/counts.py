c5a01140449fefdd7b41fbf1dab2c6a2a32e6fdd
Timothy Cronin <tcroniniv@gmail.com>
2024-12-01 23:39:01 -0500

    add flop_forward func

--- evaluation/counts.py
+++ evaluation/counts.py
@@ -6,6 +6,21 @@ from typing import Tuple
 def learnable_param_count(mod: nn.Module) -> int:
     return sum(p.numel() for p in mod.parameters() if p.requires_grad)
 
+def flop_forward(mod, input_size: Tuple):
+    istrain = mod.training
+    mod.eval()
+
+    input = torch.randn(input_size)
+
+    flop_counter = FlopCounterMode(display=False)
+    with flop_counter:
+        _ = mod(input)
+    total_flops =  flop_counter.get_total_flops()
+    if istrain:
+        mod.train()
+    return total_flops
+
+
 def flop_backward(mod, input_size: Tuple):
     istrain = mod.training
     mod.eval()
